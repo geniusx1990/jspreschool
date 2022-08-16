06cc252a34245c0becfab12e112eff3d9db28336
geniusx1990 <geniusx1990@gmail.com>
2022-08-16 19:29:35 +0300

    feat: almost done

---
+++ momentum/js/index.js
@@ -0,0 +1,14 @@
+/* import playList from './playList.js';
+ const listOfItems = document.querySelector('.play-list')
+
+for (let i = 0; i < playList.length; i++) {
+    const li = document.createElement('li');
+    li.classList.add('play-item');
+    li.textContent = playList[i].title;
+    listOfItems.append(li);
+}
+
+
+
+
+ */
